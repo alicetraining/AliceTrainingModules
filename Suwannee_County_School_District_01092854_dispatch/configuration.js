a7ebb10fc4ccbf0c99cf522b21199fb46ddd3a11
alicetraining <108097749+alicetraining@users.noreply.github.com>
2022-06-23 17:39:52 -0400

    update1

---
+++ Suwannee_County_School_District_01092854_dispatch/configuration.js
@@ -0,0 +1,6 @@
+DispatchRoot='https://cloud.scorm.com/ScormEngineInterface/dispatch/';
+ContentURL='http://cloud.scorm.com/EngineWebServices/api/cloud/dispatch/launch?dispatchId=59b97d4e-50b2-454f-a7b7-a0d68dd6f7c4&launchtoken=5LbqIBPBq7pORWMqzAOIEC7kD9v6WHRfpqi7PcdP&learnerid=LEARNER_ID&fname=LEARNER_FNAME&lname=LEARNER_LNAME&referringurl=REFERRING_URL&pipeurl=PIPE_URL&redirecturl=REDIRECT_URL&cssurl=CSS_URL_REGISTRATION_ARGUMENT';
+DefaultCssUrl='';
+DispatchVersion=2;
+TcapiEndpoint='https://cloud.scorm.com/ScormEngineInterface/TCAPI/1O71AUX42W';
+PreLaunchConfigurationURL='http://cloud.scorm.com/EngineWebServices/api/cloud/dispatch/launch/config?dispatchId=59b97d4e-50b2-454f-a7b7-a0d68dd6f7c4&launchtoken=5LbqIBPBq7pORWMqzAOIEC7kD9v6WHRfpqi7PcdP';
