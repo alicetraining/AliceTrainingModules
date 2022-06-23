a9059450893828680a0e585c69cb25eed7d119bf
alicetraining <108097749+alicetraining@users.noreply.github.com>
2022-06-23 17:16:32 -0400

    Add files via upload

---
+++ configuration.js
@@ -0,0 +1,6 @@
+DispatchRoot='https://cloud.scorm.com/ScormEngineInterface/dispatch/';
+ContentURL='http://cloud.scorm.com/EngineWebServices/api/cloud/dispatch/launch?dispatchId=71fd84bf-a1ea-4d93-baf0-a71f774b39f4&launchtoken=jro9ajI8N2Ajq7nebpC5S1FFRFsedBrlBeK8dYff&learnerid=LEARNER_ID&fname=LEARNER_FNAME&lname=LEARNER_LNAME&referringurl=REFERRING_URL&pipeurl=PIPE_URL&redirecturl=REDIRECT_URL&cssurl=CSS_URL_REGISTRATION_ARGUMENT';
+DefaultCssUrl='';
+DispatchVersion=2;
+TcapiEndpoint='https://cloud.scorm.com/ScormEngineInterface/TCAPI/1O71AUX42W';
+PreLaunchConfigurationURL='http://cloud.scorm.com/EngineWebServices/api/cloud/dispatch/launch/config?dispatchId=71fd84bf-a1ea-4d93-baf0-a71f774b39f4&launchtoken=jro9ajI8N2Ajq7nebpC5S1FFRFsedBrlBeK8dYff';
